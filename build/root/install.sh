5265b628eb87998d44a8ba510b1b861803d3c46d
overbyrn <richmsubs@gmail.com>
2018-12-27 17:25:01 +0000

    initial commit

---
+++ build/root/install.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# construct yesterdays date (cannot use todays as archive wont exist) and set url for archive
+yesterdays_date=$(date -d "yesterday" +%Y/%m/%d)
+
+# now set pacman to use snapshot for packages for yesterdays date
+echo 'Server = https://archive.archlinux.org/repos/'"${yesterdays_date}"'/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+echo 'Server = http://archive.virtapi.org/repos/'"${yesterdays_date}"'/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
+
+echo "[info] content of arch mirrorlist file"
+cat /etc/pacman.d/mirrorlist
+
+# reset gpg (not required when source is bootstrap tarball, but keeping for historic reasons)
+rm -rf /etc/pacman.d/gnupg/ /root/.gnupg/ || true
+
+# refresh gpg keys
+gpg --refresh-keys
+
+# initialise key for pacman and populate keys 
+pacman-key --init && pacman-key --populate archlinux
+
+# force use of protocol http and ipv4 only for keyserver (defaults to hkp)
+echo "no-greeting" > /etc/pacman.d/gnupg/gpg.conf
+echo "no-permission-warning" >> /etc/pacman.d/gnupg/gpg.conf
+echo "lock-never" >> /etc/pacman.d/gnupg/gpg.conf
+echo "keyserver hkp://ipv4.pool.sks-keyservers.net" >> /etc/pacman.d/gnupg/gpg.conf
+echo "keyserver-options timeout=10" >> /etc/pacman.d/gnupg/gpg.conf
+
+# refresh keys for pacman
+pacman-key --refresh-keys
+
+# force pacman db refresh and install sed package (used to do package folder exclusions)
+pacman -Sy sed --noconfirm
+
+# configure pacman to not extract certain folders from packages being installed
+# this is done as we strip out locale, man, docs etc when we build the arch-scratch image
+sed -i '\~\[options\]~a # Do not extract the following folders from any packages being installed\n'\
+'NoExtract   = usr/share/locale* !usr/share/locale/en* !usr/share/locale/locale.alias\n'\
+'NoExtract   = usr/share/doc*\n'\
+'NoExtract   = usr/share/man*\n'\
+'NoExtract   = usr/share/gtk-doc*\n' \
+/etc/pacman.conf
+
+# list all packages that we want to exclude/remove
+unneeded_packages="\
+filesystem \
+cryptsetup \
+device-mapper \
+dhcpcd \
+iproute2 \
+jfsutils \
+libsystemd \
+linux \
+lvm2 \
+man-db \
+man-pages \
+mdadm \
+netctl \
+openresolv \
+pciutils \
+pcmciautils \
+reiserfsprogs \
+s-nail \
+systemd \
+systemd-sysvcompat \
+usbutils \
+xfsprogs"
+
+# split space separated string into list for install paths
+IFS=' ' read -ra unneeded_packages_list <<< "${unneeded_packages}"
+
+# construct string to ensure removal of any packages that might be part of tarball
+pacman_remove_unneeded_packages='pacman --noconfirm -Rsc'
+
+for i in "${unneeded_packages_list[@]}"; do
+
+    pacman_remove_unneeded_packages="${pacman_remove_unneeded_packages} ${i}"
+
+done
+
+echo "[info] Removing unneeded packages that might be part of the tarball..."
+echo "${pacman_remove_unneeded_packages} || true"
+eval "${pacman_remove_unneeded_packages} || true"
+
+# update packages currently installed
+pacman -Syu --noconfirm
+
+# install grep package (used to do package install exclusions)
+pacman -S grep --noconfirm
+
+# construct string to install base excluding unneeded_packages
+pacman_base_install='pacman --noconfirm -S $(pacman -Sgq base |'
+
+for i in "${unneeded_packages_list[@]}"; do
+
+    pacman_base_install="${pacman_base_install} grep -v ${i} |"
+
+done
+
+# remove end pipe and close bracket instead - important required sed -E for extended regex
+pacman_base_install=$(echo "${pacman_base_install}" | sed -E 's~\s\|$~)~')
+
+echo "[info] Install base group packages with exclusions..."
+echo "${pacman_base_install} || true"
+eval "${pacman_base_install} || true"
+
+# install additional packages
+pacman -S awk sed supervisor nano vi ldns moreutils net-tools dos2unix unzip unrar htop jq openssl-1.0 --noconfirm
+
+# set locale
+echo en_GB.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen
+echo LANG="en_GB.UTF-8" > /etc/locale.conf
+
+# add user "nobody" to primary group "users" (will remove any other group membership)
+usermod -g users nobody
+
+# add user "nobody" to secondary group "nobody" (will retain primary membership)
+usermod -a -G nobody nobody
+
+# setup env for user nobody
+mkdir -p /home/nobody
+chown -R nobody:users /home/nobody
+chmod -R 775 /home/nobody
+
+# set user "nobody" home directory (needs defining for pycharm, and possibly other apps)
+usermod -d /home/nobody nobody
+ 
+# set shell for user nobody
+chsh -s /bin/bash nobody
+ 
+# force re-install of ncurses 6.x with 5.x backwards compatibility (can be removed onced all apps have switched over to ncurses 6.x)
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/ncurses5-compat.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/ncurses5-compat-libs-6.0+20161224-1-x86_64.pkg.tar.xz
+pacman -U /tmp/ncurses5-compat.tar.xz --noconfirm
+
+# find latest tini release tag from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/tini_release_tag -L https://github.com/krallin/tini/releases
+tini_release_tag=$(cat /tmp/tini_release_tag | grep -P -o -m 1 '(?<=/krallin/tini/releases/tag/)[^"]+')
+
+# download tini, used to do graceful exit when docker stop issued and correct reaping of zombie processes.
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /usr/bin/tini -L "https://github.com/krallin/tini/releases/download/${tini_release_tag}/tini-amd64" && chmod +x /usr/bin/tini
+
+# cleanup
+yes|pacman -Scc
+rm -rf \
+/usr/share/locale/* \
+/usr/share/man/* \
+/usr/share/gtk-doc/* \
+/root/* \
+/tmp/* \
+/var/cache/pacman/pkg/* \
+/README
